a932c4c7aeb6d1f2504e74000c1d014b92ad4c6b
Matt Jones <jonesmr+github@gmail.com>
2012-12-15 20:15:22 +0000

    protoype much simpler programming model, but still with some hacks

--- extension/uploader.js
+++ extension/uploader.js
@@ -38,7 +38,7 @@ function parseHexfile(url) {
 
 function uploadCompiledSketch(hexData, serialPortName) {
   console.log("Uploading to: " + serialPortName);
-  chrome.serial.open(serialPortName, { bitrate: 57600 }, uploadOpenDone);
+  chrome.serial.open(serialPortName, { bitrate: 57600 }, uploadOpenDone2);
 }
 
 function uploadOpenDone(openArg) {
@@ -58,10 +58,114 @@ function drain(connectionId) {
 
 ////////////
 
+// Consume from STK_INSYNC, until STK_OK
+
+var ReadState = {
+  WAITING_FOR_IN_SYNC: 0,
+  CONSUMING_PAYLOAD: 1,
+  DONE: 2,
+  ERROR: 3,
+};
+
+function consumeMessage(connectionId, callback) {
+  var accum = [];
+  var state = ReadState.WAITING_FOR_IN_SYNC;
+  var kMaxReads = 100;
+  var reads = 0;
+
+  var handleRead = function(arg) {
+    if (reads++ >= kMaxReads) {
+      console.log("Too many reads. Bailing.");
+      return;
+    }
+    var hexData = binToHex(arg.data);
+    if (arg.bytesRead > 0) {
+      console.log("[" + connectionId + "] Read: " + hexData);
+    }
+    for (var i = 0; i < hexData.length; ++i) {
+      if (state == ReadState.WAITING_FOR_IN_SYNC) {
+        if (hexData[i] == STK_INSYNC) {
+          state = ReadState.CONSUMING_PAYLOAD;
+        } else {
+          console.log("Expected STK_INSYNC. Got: " + hexData[i]);
+          state = ReadState.ERROR;
+        }
+      } else if (state == ReadState.CONSUMING_PAYLOAD) {
+        if (hexData[i] == STK_OK) {
+          state = ReadState.DONE;
+        } else {
+          accum.push(hexData[i]);
+          // Remains in state CONSUMING_PAYLOAD
+        }
+      } else if (state == ReadState.DONE) {
+        console.log("Out of sync");
+        state = ReadState.ERROR;
+      } else if (state == ReadState.ERROR) {
+        console.log("In error state. Draining byte: " + hexData[i]);
+        // Remains in state ERROR
+      } else {
+        console.log("Unknown state: " + state);
+        state = ReadState.ERROR;
+      }
+    }
+
+    if (state == ReadState.ERROR || state == ReadState.DONE) {
+      console.log("Finished in state: " + state);
+      callback(connectionId, state == ReadState.DONE, accum);
+    } else {
+      console.log("Paused in state: " + state + ". Reading again.");
+
+      // Mega hack (temporary)
+      chrome.serial.write(connectionId, hexToBin([STK_GET_SYNC, STK_CRC_EOP]), function() { });
+
+      // Don't tight-loop waiting for the message.
+      setTimeout(function() { chrome.serial.read(connectionId, 1024, handleRead); }, 100);
+    }
+  };
+
+  console.log("Scheduling a read in 1s");
+  setTimeout(function() { chrome.serial.read(connectionId, 1024, handleRead); }, 1000);
+//  chrome.serial.read(connectionId, 1024, handleRead);
+}
+
+function writeThenRead(connectionId, outgoingMsg, callback) {
+  console.log("[" + connectionId + "] Writing: " + outgoingMsg);
+  var outgoingBinary = hexToBin(outgoingMsg);
+  // schedule a read in 100ms
+  chrome.serial.write(connectionId, outgoingBinary, function(writeArg) {
+      consumeMessage(connectionId, callback);
+    });
+}
+
+function uploadOpenDone2(openArg) {
+  if (openArg.connectionId == -1) {
+    console.log("Couldn't connect to board");
+    return;
+  }
+
+  chrome.serial.read(openArg.connectionId, 1024, function(readArg) {
+      // Drain the connection
+      console.log("DRAINED " + readArg.bytesRead + " BYTES");
+
+      writeThenRead(openArg.connectionId, [STK_GET_SYNC, STK_CRC_EOP], inSyncWithBoard);
+    });
+}
+
+function inSyncWithBoard(connectionId, ok, data) {
+  console.log("InSyncWithBoard: " + ok + " / " + data);
+
+  writeThenRead(connectionId, [STK_GET_PARAMETER, STK_HW_VER, STK_CRC_EOP], readHardwareVersion);
+}
+
+function readHardwareVersion(connectionId, ok, data) {
+  console.log("ReadHardwareVersion: " + ok + " / " + data);
+}
+
 function waitForSync(connectionId) {
   console.log("readying sync bit from: " + connectionId);
-  var data = hexToBin([STK_GET_SYNC, STK_CRC_EOP]);
-  console.log("writing: " + data);
+  var hex = [STK_GET_SYNC, STK_CRC_EOP];
+  var data = hexToBin(hex)
+  console.log("writing: " + hex + " -> " + data);
   chrome.serial.write(connectionId, data, function(arg) { checkSync(connectionId, arg) } );
 }
 
@@ -76,7 +180,7 @@ function checkSync(connectionId, sendArg) {
 function checkSyncReadDone(connectionId, arg) {
   console.log(JSON.stringify(arg));
   if (arg.bytesRead != 2) {
-    console.log("Read wrong length data: " + binToHex(arg.data));
+    console.log("Read wrong length data. Expected 2 got: " + arg.bytesRead + " -- " + binToHex(arg.data));
     setTimeout(function() { drain(connectionId) }, 100);
     return;
   }
