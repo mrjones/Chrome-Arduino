cfacc96392287990b19eed9ab3ab729c3a68d83d
Matt Jones <jonesmr@gmail.com>
2012-12-20 09:20:38 -0500

    made hex-parser it's own file

--- extension/uploader_test.js
+++ extension/uploader_test.js
@@ -1,19 +1,5 @@
 var UploaderTest = {
-  hexFileTest: function() {
-    var input =
-      ":100000000C9461000C947E000C947E000C947E0095 \n" +
-      ":00000001FF \n\n";
-
-    var expectedOutput =
-      [ 0x0c, 0x94, 0x61, 0x00, 0x0C, 0x94, 0x7E, 0x00, 0x0C, 0x94, 0x7E, 0x00, 0x0C, 0x94, 0x7E, 0x00];
-
-    var actualOutput = parseHexFile(input);
-    if (!Test.ArrayEquals(expectedOutput, actualOutput)) {
-        return {passed: false, message: "Actual:   " + actualOutput + "\nExpected: " + expectedOutput};
-    } else {
-      return {passed: true};
-    }
-  }
+  pass: function() { return {passed: true}; }
 };
 
 Test.Run("UploaderTest", UploaderTest);
