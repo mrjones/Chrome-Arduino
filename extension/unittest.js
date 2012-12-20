c4ece7886ce8f100b02743f4abded9fd3fa98397
Matt Jones <jonesmr@gmail.com>
2012-12-20 09:09:35 -0500

    first real passing test

--- extension/unittest.js
+++ extension/unittest.js
@@ -1,26 +1,44 @@
-function RunTest(testCaseName, testCase) {
-  var parentResultsDiv = document.getElementById('results');
-  var testResultsDiv = document.createElement('div');
-  var testNameDiv = document.createElement('div');
-  var testMessageDiv = document.createElement('div');
-  testResultsDiv.appendChild(testNameDiv);
-  testResultsDiv.appendChild(testMessageDiv);
-  parentResultsDiv.appendChild(testResultsDiv);
-  testNameDiv.innerText = testCaseName + " - RUNNING";
-  testResultsDiv.style.border = "1px solid black";
+var Test = {
+  Run: function(testCaseName, testCase) {
+    var parentResultsDiv = document.getElementById('results');
+    var testResultsDiv = document.createElement('div');
+    var testNameDiv = document.createElement('div');
+    var testMessageDiv = document.createElement('div');
+    testResultsDiv.appendChild(testNameDiv);
+    testResultsDiv.appendChild(testMessageDiv);
+    parentResultsDiv.appendChild(testResultsDiv);
+    testNameDiv.innerText = testCaseName + " - RUNNING";
+    testResultsDiv.style.border = "1px solid black";
 
-  var result = execute(testCase);
-  if (result.passed) {
-    testNameDiv.style.color = 'green';
-    testNameDiv.innerText = testCaseName + " - PASSED";
-  } else {
-    testNameDiv.style.color = 'red';
-    testNameDiv.innerText = testCaseName + " - FAILED";
-  }
-  for (var i = 0; i < result.messages.length; ++i) { 
-    var messageDiv = document.createElement('div');
-    testMessageDiv.appendChild(messageDiv);
-    testMessageDiv.innerText += result.messages[i];
+    var result = execute(testCase);
+    if (result.passed) {
+        testNameDiv.style.color = 'green';
+        testNameDiv.innerText = testCaseName + " - PASSED";
+    } else {
+        testNameDiv.style.color = 'red';
+        testNameDiv.innerText = testCaseName + " - FAILED";
+    }
+    for (var i = 0; i < result.messages.length; ++i) { 
+        var messageDiv = document.createElement('div');
+        testMessageDiv.appendChild(messageDiv);
+        var messagePre = document.createElement('pre');
+        messageDiv.appendChild(messagePre);
+        messagePre.innerText += result.messages[i];
+    }
+  },
+
+  ArrayEquals: function(a1, a2) {
+    if (a1.length != a2.length) {
+      return false;
+    }
+
+    for (var i = 0; i < a1.length; ++i) {
+      if (a1[i] != a2[i]) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
 
--- extension/uploader_test.js
+++ extension/uploader_test.js
@@ -1,25 +1,19 @@
 var UploaderTest = {
-  pass: function() {
-    return {passed: true, message: null};
-  },
-  fail: function() {
-    return {passed: false, message: "hard-coded to fail"};
-  },
   hexFileTest: function() {
     var input =
-      ":100000000C9461000C947E000C947E000C947E0095\n" +
-      ":00000001FF\n\n";
+      ":100000000C9461000C947E000C947E000C947E0095 \n" +
+      ":00000001FF \n\n";
 
     var expectedOutput =
-    [ 0x0c, 0x94, 0x61, 0x00, 0x0C, 0x94, 0x7E, 0x00, 0x0C, 0x94, 0x7E, 0x00, 0x0C, 0x94, 0x7E, 0x00];
+      [ 0x0c, 0x94, 0x61, 0x00, 0x0C, 0x94, 0x7E, 0x00, 0x0C, 0x94, 0x7E, 0x00, 0x0C, 0x94, 0x7E, 0x00];
 
     var actualOutput = parseHexFile(input);
-    if (expectedOutput != actualOutput) {
-      return {passed: false, message: "Actual: " + actualOutput + ", Expected: " + expectedOutput};
+    if (!Test.ArrayEquals(expectedOutput, actualOutput)) {
+        return {passed: false, message: "Actual:   " + actualOutput + "\nExpected: " + expectedOutput};
     } else {
       return {passed: true};
     }
   }
 };
 
-RunTest("UploaderTest", UploaderTest);
+Test.Run("UploaderTest", UploaderTest);
