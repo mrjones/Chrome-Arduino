b372d3aae6206dbc6d5667232dc7e573c772b418
Matt Jones <jonesmr+github@gmail.com>
2012-12-22 19:25:47 +0000

    removed dead code in uploader.js

--- extension/uploader.js
+++ extension/uploader.js
@@ -53,21 +53,6 @@ function uploadCompiledSketch(hexData, serialPortName) {
   chrome.serial.open(serialPortName, { bitrate: 57600 }, uploadOpenDone2);
 }
 
-function uploadOpenDone(openArg) {
-  if (openArg.connectionId == -1) {
-    console.log("Couldn't connect to board");
-    return;
-  }
-  drain(openArg.connectionId);
-}
-
-function drain(connectionId) {
-  chrome.serial.read(connectionId, 1024, function(readArg) {
-      console.log("DRAINED " + readArg.bytesRead + " BYTES");
-      waitForSync(connectionId);
-    });
-}
-
 ////////////
 
 // Consume from STK_INSYNC, until STK_OK
@@ -283,75 +268,6 @@ function waitForSync(connectionId) {
   console.log("writing: " + hex + " -> " + data);
   chrome.serial.write(connectionId, data, function(arg) { checkSync(connectionId, arg) } );
 }
-
-function checkSync(connectionId, sendArg) {
-  setTimeout(function() {
-      chrome.serial.read(connectionId, 1024, function(arg) {
-          checkSyncReadDone(connectionId, arg);
-        });
-    }, 1000);
-}
-
-function checkSyncReadDone(connectionId, arg) {
-  console.log(JSON.stringify(arg));
-  if (arg.bytesRead != 2) {
-    console.log("Read wrong length data. Expected 2 got: " + arg.bytesRead + " -- " + binToHex(arg.data));
-    setTimeout(function() { drain(connectionId) }, 100);
-    return;
-  }
-
-  var data = binToHex(arg.data);
-  console.log("READ: " + data + "/" + data.length);
-  if (data[0] != STK_INSYNC) {
-    console.log("Didn't get right response");
-    return waitForSync(connectionId);
-  }
-
-  if (data[1] != STK_OK) {
-    console.log("Didn't get STK_OK");
-    return;
-  }
-
-  console.log("CONNECTED");
-  getProtocolVersion(connectionId);
-}
-
-//////////////
-
-function getProtocolVersion(connectionId) {
-  
-  chrome.serial.write(connectionId, hexToBin([STK_GET_PARAMETER, STK_HW_VER, STK_CRC_EOP]), function(wArg) {
-      setTimeout(function() {
-          chrome.serial.read(connectionId, 1024, function(rArg) {
-              parseProtocolVersion(connectionId, rArg);
-            });
-        }, 1000);
-    });
-}
-
-function parseProtocolVersion(connectionId, readArg) {
-  console.log("Parsing protocol version. Read: " + readArg.bytesRead + " bytes .");
-  if (readArg.bytesRead != 3) {
-    console.log("Wrong number of bytes in response");
-    return;
-  }
-
-  var data = binToHex(readArg.data);
-  if (data[0] != STK_INSYNC) {
-    console.log("Data not in sync. Consumed: " + data[0] + " as byte 0 of " + data);
-    return;
-  }
-
-  console.log("Hardware version: " + data[1]);
-
-  if (data[2] != STK_OK) {
-    console.log("didn't get STK_OK. Consumed " + data[2] + " as byte 2 of " + data);
-    return;
-  }
-}
-
-
-// Implementation
 
 function hexToBin(hex) {
   var buffer = new ArrayBuffer(hex.length);
