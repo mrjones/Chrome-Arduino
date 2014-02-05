a3897a84c554dcb793982b0c9826c9206dc9e8cb
Matt Jones <jonesmr+github@gmail.com>
2014-02-05 15:07:27 +0000

    very simple test with jasmine

--- test/uploader_test.js
+++ test/uploader_test.js
@@ -1,3 +1,23 @@
+var FakeSerial = {
+  open: function(portname, options, doneCb) {
+    console.log("Setting portname_ to " + portname);
+    portname_ = portname;
+  },
+
+  portname_: null,
+
+  getPortname: function() {
+    return portname_;
+  }
+};
+
+var chrome = {
+  serial: FakeSerial,
+};
+
 describe("Uploader", function() {
-  it("Passes", function() { } );
+  it("opens serial port", function() {
+    uploadCompiledSketch([0x01, 0x02, 0x03, 0x04], "portname", "stk500");
+    expect(FakeSerial.getPortname()).toBe("portname");
+  });
 });
