8851992b62cc4834e95c68f704fa2c6cafff17b3
Matt Jones <jonesmr+github@gmail.com>
2014-02-06 01:12:51 +0000

    error checking in fakeserial

--- test/fakeserial.js
+++ test/fakeserial.js
@@ -1,16 +1,23 @@
 var FakeSerial = {
   open: function(portname, options, doneCb) {
     portname_ = portname;
-    FakeSerial.execute_(doneCb, { connectionId: FakeSerial.connection_id_++ });
+    FakeSerial.execute_(doneCb, { connectionId: FakeSerial.connection_id_ });
   },
 
   read: function(connectionId, n, doneCb) {
+    if (connectionId != FakeSerial.connection_id_) {
+      FakeSerial.errors_.push("Mismatched connection ID. Expected: '" +
+                              FakeSerial.connection_id_ + "'. Actual: '" +
+                              connectionId + "'");
+      return;
+    }
     console.log("FakeSerial read of " + n + " bytes on connection " + connectionId);
     // TODO
   },
 
   portname_: null,
-  connection_id_: 0,
+  connection_id_: 123456,
+  errors_: [],
 
   getPortname: function() {
     return portname_;
@@ -20,6 +27,7 @@ var FakeSerial = {
   execute_: function(callback, args) {
     callback(args);
   },
+
 
 };
 
--- test/uploader_test.js
+++ test/uploader_test.js
@@ -1,6 +1,11 @@
 describe("Uploader", function() {
+  afterEach(function() {
+    expect(FakeSerial.errors_).toEqual([]);    
+  });
+
   it("opens serial port", function() {
     uploadCompiledSketch([0x01, 0x02, 0x03, 0x04], "portname", "stk500");
     expect(FakeSerial.getPortname()).toBe("portname");
   });
+
 });
