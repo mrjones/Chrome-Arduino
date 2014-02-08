364e01e8e34cbcaa9d563f65ab129a0aa62184db
Matt Jones <jonesmr+github@gmail.com>
2014-02-08 19:15:57 +0000

    separated status to own file

--- extension/board.js
+++ extension/board.js
@@ -4,23 +4,6 @@
 // ReadFlash
 // WriteFlash
 
-function Status(ok, errorMessage) {
-  this.ok_ = ok;
-  this.errorMessage_ = errorMessage;
-};
-
-Status.prototype.ok = function() { return this.ok_; }
-Status.prototype.errorMessage = function() { return this.errorMessage_; }
-
-Status.OK = function() {
-  return new Status(true, null);
-}
-
-Status.Error = function(message) {
-  return new Status(false, message);
-}
-
-
 function Stk500Board() {
   
 };
---
+++ extension/status.js
@@ -0,0 +1,15 @@
+function Status(ok, errorMessage) {
+  this.ok_ = ok;
+  this.errorMessage_ = errorMessage;
+};
+
+Status.prototype.ok = function() { return this.ok_; }
+Status.prototype.errorMessage = function() { return this.errorMessage_; }
+
+Status.OK = function() {
+  return new Status(true, null);
+}
+
+Status.Error = function(message) {
+  return new Status(false, message);
+}
