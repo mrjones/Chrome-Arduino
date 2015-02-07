2777fe100021690f0cba2a0f704cb2f9150054f3
Matt Jones <jonesmr@gmail.com>
2015-02-07 21:30:10 +0000

    partially implemented writeflash test

--- extension2/test/fakestk500.js
+++ extension2/test/fakestk500.js
@@ -1,9 +1,11 @@
 var binary = require("../lib/binary.js");
 var STK = require("../lib/stk500.js").STK;
 
-var FakeStk500 = function() {
+var FakeStk500 = function(memorySize) {
   this.onReceive.addListener = this.addListenerImpl_.bind(this);
   this.onReceive.removeListener = this.removeListenerImpl_.bind(this);
+
+  this.memory_ = new Array(memorySize);
 }
 
 // Chrome Serial API
@@ -12,7 +14,7 @@ FakeStk500.prototype.connect = function(deviceName, options, done) {
 }
 
 FakeStk500.prototype.disconnect = function(connectionId, done) {
-  this.disconnectImpl_(deviceName, options, done);
+  this.disconnectImpl_(connectionId, done);
 }
 
 FakeStk500.prototype.send = function(connectionId, payload, done) {
@@ -38,11 +40,14 @@ FakeStk500.BootloaderState = {
   IN_BOOTLOADER: 2,
 };
 
+FakeStk500.prototype.addressPtr_ = -1;
+FakeStk500.prototype.bootloaderState_ = FakeStk500.BootloaderState.NOT_IN_BOOTLOADER;
 FakeStk500.prototype.connectionId_ = -1;
+FakeStk500.prototype.inProgmode_ = false;
+FakeStk500.prototype.listeners_ = [];
+FakeStk500.prototype.memory_ = null;  // Initialized in constructor
 FakeStk500.prototype.nextConnectionId_ = 10;
 FakeStk500.prototype.serialConnected_ = false;
-FakeStk500.prototype.bootloaderState_ = FakeStk500.BootloaderState.NOT_IN_BOOTLOADER;
-FakeStk500.prototype.listeners_ = [];
 
 FakeStk500.prototype.connectImpl_ = function(deviceName, options, done) {
   this.serialConnected_ = true;
@@ -52,7 +57,15 @@ FakeStk500.prototype.connectImpl_ = function(deviceName, options, done) {
 }
 
 FakeStk500.prototype.disconnectImpl_ = function(connectionId, done) {
-
+  if (this.connectionId_ == connectionId) {
+    this.connectiondId_ = -1;
+    this.serialConnected_ = false;
+    this.bootloaderState_ = FakeStk500.BootloaderState.NOT_IN_BOOTLOADER;
+    this.inProgmode = false;
+    done(true);
+  } else {
+    done(false); 
+  }
 }
 
 var arraysEqual = function(a1, a2) {
@@ -62,6 +75,20 @@ var arraysEqual = function(a1, a2) {
 
   for (var i = 0; i < a1.length; i++) {
     if (a1[i] != a2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+var hasPrefix = function(candidate, prefix) {
+  if (candidate.length < prefix.length) {
+    return false;
+  }
+
+  for (var i = 0; i < prefix.length; i++) {
+    if (prefix[i] != candidate[i]) {
       return false;
     }
   }
@@ -94,8 +121,54 @@ FakeStk500.prototype.sendImpl_ = function(connectionId, binaryPayload, done) {
 
   // TODO(mrjones): We could make GET_PARAMETER more general.
   if (arraysEqual(payload, [STK.GET_PARAMETER, STK.HW_VER, STK.CRC_EOP])) {
-    done({bytesSend: payload.length});
+    done({bytesSent: payload.length});
     this.sendReply_([STK.IN_SYNC, 3, STK.OK]);
+    return;
+  }
+
+  if (arraysEqual(payload, [STK.ENTER_PROGMODE, STK.CRC_EOP])) {
+    done({bytesSent: payload.length});
+    this.inProgmode_ = true;
+    this.sendReply_([STK.IN_SYNC, STK.OK]);
+    return;
+  }
+
+  if (arraysEqual(payload, [STK.LEAVE_PROGMODE, STK.CRC_EOP])) {
+    done({bytesSent: payload.length});
+    this.inProgmode_ = false;
+    this.sendReply_([STK.IN_SYNC, STK.OK]);
+    return;
+  }
+
+  if (hasPrefix(payload, [STK.LOAD_ADDRESS]) && payload.length == 4 &&
+      payload[3] == STK.CRC_EOP) {
+    done({bytesSent: payload.length});
+    var wordAddress = (payload[2] << 8) + payload[1];
+    var byteAddress = wordAddress * STK.BYTES_PER_WORD;
+    this.addressPtr_ = byteAddress;
+    this.sendReply_([STK.IN_SYNC, STK.OK]);
+    return;
+  }
+
+  if (hasPrefix(payload, [STK.PROG_PAGE]) && payload.length >= 5) {
+    var length = (payload[1] << 8) + payload[2];
+    // TODO(mrjones): verify in PROG_MODE
+    // TODO(mrjones): verify addressPtr
+    // TODO(mrjones): Support different kinds of memory??
+    // TODO(mrjones): verify that payload[3] == STK.FLASH_MEMORY
+    // TODO(mrjones): verify that payload[<last>] == STK.CRC_EOP
+    if (length + 5 != payload.length) {
+      console.log("Bad PROG_PAGE command. " + length + " + 5 != " + payload.length);
+      return;
+    }
+
+    done({bytesSent: payload.length});
+
+    for (var i = 0; i < length; i++) {
+      this.memory_[i + this.addressPtr_] = payload[i + 4];
+    }
+
+    this.sendReply_([STK.IN_SYNC, STK.OK]);
     return;
   }
 }
--- extension2/test/stk500_test.js
+++ extension2/test/stk500_test.js
@@ -2,6 +2,17 @@ var assert = require("assert")
 var stk500 = require("../lib/stk500.js")
 var FakeStk500 = require("./fakestk500.js").FakeStk500;
 
+var payloadPattern = [ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09 ];
+
+var genPayload = function(length) {
+  var payload = new Array(length);
+  for (var i = 0; i < length; i++) {
+    payload[i] = payloadPattern[i % payloadPattern.length];
+  }
+
+  return payload;
+}
+
 describe("stk500", function() {
   it("doesn't write until connected", function(done) {
     var result = stk500.NewStk500Board(null, 1024);
@@ -22,6 +33,26 @@ describe("stk500", function() {
     result.board.connect("devicename", function(connectStatus) {
       assert.equal(true, connectStatus.ok(), connectStatus.toString());
       done();
+    });
+  });
+
+  it("writesFlash", function(done) {
+    var fake = new FakeStk500();
+    var result = stk500.NewStk500Board(fake, 128, {connectDelayMs: 10});
+
+    assert.equal(true, result.status.ok(), result.status.toString());
+
+    result.board.connect("devicename", function(connectStatus) {
+      assert.equal(true, connectStatus.ok(), connectStatus.toString());
+
+      result.board.writeFlash(0, genPayload(256), function(writeStatus) {
+        assert.equal(true, connectStatus.ok(), connectStatus.toString());
+
+        // TODO(mrjones): verify the flash memory.
+        // Ideally we'd do this with read flash, but for now maybe we just ask
+        // the fake directly.
+        done();
+      });
     });
   });
 });
