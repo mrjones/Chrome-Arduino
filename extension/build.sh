2daceca60ec7ce56f816a4efe0dfd224764e4f35
Matt Jones <jonesmr@gmail.com>
2015-02-08 23:08:06 +0000

    update build.sh

--- extension/build.sh
+++ extension/build.sh
@@ -6,4 +6,6 @@
 # sudo npm install -g mocha
 grunt
 
-xvfb-run google-chrome --pack-extension=./extension/ --pack-extension-key=./chrome-arduino-extension.pem --enable-experimental-extension-apis
+xvfb-run google-chrome --pack-extension=./deploy/ --pack-extension-key=./chrome-arduino-extension.pem --enable-experimental-extension-apis
+
+mv -f deploy.crx extension.crx
