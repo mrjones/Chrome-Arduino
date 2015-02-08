bdd0e7473a995bf2d77794302efe7fc22a370202
Matt Jones <jonesmr@gmail.com>
2015-02-08 17:00:39 +0000

    removed unused logic.js

--- extension2/test/logic_test.js
+++
@@ -1,8 +0,0 @@
-var assert = require("assert");
-var logic = require("../lib/logic");
-
-describe("add", function() {
-  it("adds 2 and 2", function() {
-    assert.equal(logic.add(2, 2), 4);
-  });
-});
